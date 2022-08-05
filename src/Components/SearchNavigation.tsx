3378ab3621088e1322f1189d169b581185cdba80
Daniel Vazač <daniel.vazac@email.cz>
2022-08-05 20:50:00 +0200

    CenterNav

--- src/Components/SearchNavigation.tsx
+++ src/Components/SearchNavigation.tsx
@@ -3,22 +3,10 @@ import { Container, Pagination } from "react-bootstrap";
 function SearchNavigation() {
   return (
     <Container>
-      <Pagination>
-        <Pagination.First />
+      <Pagination className="justify-content-center">
         <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
-
-        <Pagination.Item>{10}</Pagination.Item>
-        <Pagination.Item>{11}</Pagination.Item>
-        <Pagination.Item active>{12}</Pagination.Item>
-        <Pagination.Item>{13}</Pagination.Item>
-        <Pagination.Item disabled>{14}</Pagination.Item>
-
-        <Pagination.Ellipsis />
-        <Pagination.Item>{20}</Pagination.Item>
+        <Pagination.Item active>{1}</Pagination.Item>
         <Pagination.Next />
-        <Pagination.Last />
       </Pagination>
     </Container>
   );
