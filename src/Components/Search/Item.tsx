ae25d8997c8fd61baa4fd2e82b06d355cbb0e1c7
Daniel <daniel.vazac@email.cz>
2022-10-25 15:59:25 +0200

    !FIX buyAmount

--- src/Components/Search/Item.tsx
+++ src/Components/Search/Item.tsx
@@ -3,36 +3,35 @@ import {Button, Container} from "react-bootstrap";
 import {Item, SaveItem, SaveItems} from "../../API/Items";
 import PopUpWindow from "../PopUpWindow";
 
+
 const SearchItem = (props: { item: Item }) => {
-    let newItem: Item = new Item(props.item);
-    
     const [showDetail, setDetail] = useState(false);
-    const [itemBuylist, changeItemBuylist] = useState(newItem.buylist);
+    const [itemBuyAmount, setItemBuyAmount] = useState(props.item.amount || 0);
 
     useEffect(() => {
-        newItem.buylist = itemBuylist;
-        SaveItem(newItem);
-    }, [itemBuylist]);
+        props.item.amount = itemBuyAmount;
+        SaveItem(props.item);
+    }, [itemBuyAmount]);
 
     return (
         <Container>
 
             <div className="gridContainer">
-                <div><b>{newItem.name.toLowerCase().charAt(0).toUpperCase() + newItem.name.slice(1).toLowerCase()}</b></div>
-                <div>{newItem.price}</div>
+                <div><b>{props.item.name.toLowerCase().charAt(0).toUpperCase() + props.item.name.slice(1).toLowerCase()}</b></div>
+                <div>{props.item.price}</div>
                 <Button variant="primary" onClick={() => setDetail(true)}>Detail</Button>
-                <div>
-                    <Button variant="danger" onClick={() => changeItemBuylist(itemBuylist - 1)}>-</Button>
-                    <b>{itemBuylist}</b>
-                    <Button variant="success" onClick={() => changeItemBuylist(itemBuylist + 1)}>+</Button>
+                <div className="amountButtonsContainer">
+                    <Button className="amountButton" variant="danger" onClick={() => setItemBuyAmount(Math.max((itemBuyAmount - 1), 0))}>-</Button>
+                    <b className="amountButton">{itemBuyAmount}</b>
+                    <Button className="amountButton" variant="success" onClick={() => setItemBuyAmount(Math.max((itemBuyAmount + 1), 0))}>+</Button>
                 </div>
             </div>
 
             <PopUpWindow
                 trigger={showDetail}
                 setTrigger={setDetail}
-                title={newItem.name.replace(/^./, newItem.name[0].toUpperCase())}
-                content={`${newItem.name.replace(/^./, newItem.name[0].toUpperCase())} právě stojí ${newItem.price} Kč`}
+                title={props.item.name.replace(/^./, props.item.name[0].toUpperCase())}
+                content={`${props.item.name.replace(/^./, props.item.name[0].toUpperCase())} právě stojí ${props.item.price} Kč`}
             />
 
         </Container>
