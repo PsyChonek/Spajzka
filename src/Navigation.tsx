82d37d0218272ce9fcc8adbf84ac5dda8f8ed38b
Daniel Vazač <daniel.vazac@email.cz>
2022-11-03 17:27:19 +0100

    Navigation.tsx

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,24 +1,31 @@
-﻿import React, {Component} from 'react';
+﻿import React, {Component, useEffect} from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {Link, Navigate, Route, Routes} from "react-router-dom";
 import Spajz from "./Pages/Spajz";
 import Buylist from "./Pages/Buylist";
 
 const Navigation = () => {
+    
+    const [active, setActive] = React.useState("");
+    
+    useEffect(() => {
+        setActive(window.location.pathname.split("/")[1]);
+    }, [window.location.pathname]);
+
     return (
         <body>
         <Navbar className="navbar" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand  className="navbar-button" as={Link} to="/spajz">Špajzka</Navbar.Brand>
-                    <Nav defaultActiveKey="/spajz" className="me-auto">
-                        <Nav.Link className="navbar-link" as={Link} to="/buylist">Nákupní lístek</Nav.Link>
-                    </Nav>
+                <Navbar.Brand onClick={() => setActive('spajz')} className={active === 'spajz' ? "navbar-link-active" : "navbar-link"} as={Link} to="/spajz">Špajzka</Navbar.Brand>
+                <Nav defaultActiveKey="/spajz" className="me-auto">
+                    <Nav.Link onClick={() => setActive('buylist')} className={active === 'buylist' ? "navbar-link-active" : "navbar-link"} as={Link} to="/buylist">Nákupní lístek</Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
 
         <Routes>
             <Route path="/" element={<Navigate to="/spajz"/>}/>
-            <Route path="/spajz"  element={<Spajz/>}/>
+            <Route path="/spajz" element={<Spajz/>}/>
             <Route path="/buylist" element={<Buylist/>}/>
         </Routes>
         </body>
