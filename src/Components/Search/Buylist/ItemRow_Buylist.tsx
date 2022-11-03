a4f0bb922f0f9159c07d07fda76bcc5891ba9736
Daniel Vazač <daniel.vazac@email.cz>
2022-11-03 18:14:48 +0100

    Cross

--- src/Components/Search/Buylist/ItemRow_Buylist.tsx
+++ src/Components/Search/Buylist/ItemRow_Buylist.tsx
@@ -9,13 +9,16 @@ const ItemRow_Buylist = (props: { item: Item, updateCallback: any }) => {
         SaveItem(props.item);
         props.updateCallback();
     }
+    
+    const getRandomRotation = () => {
+        return Math.floor(Math.random() * 360);
+    }
 
     return (
         <div className="gridContainer-item-buylist" onClick={() => UpdateItem(!props.item.isOnBuylist)}>
-            {props.item.isOnBuylist ? <></> : <div className="cross"></div>}
-            <div><b>{props.item.name.toLowerCase().charAt(0).toUpperCase() + props.item.name.slice(1).toLowerCase()}</b></div>
+            {!props.item.isOnBuylist ? <div className="cross"></div> : <></>}
+            <div className={props.item.isOnBuylist ? "" : "cross-text"}><b>{props.item.name.toLowerCase().charAt(0).toUpperCase() + props.item.name.slice(1).toLowerCase()}</b></div>
             {props.item.isOnBuylist ? <>✅</> : <>❌</>}
-            {/*<Form.Check type={"checkbox"} checked={props.item.isOnBuylist}></Form.Check>*/}
         </div>
     );
 }
