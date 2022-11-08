44c3de72d3143d101cc765cc094220177706308d
Daniel <daniel.vazac@email.cz>
2022-11-08 15:36:59 +0100

    !FIX Navigation outside

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -5,31 +5,40 @@ import Spajz from "./Pages/Spajz";
 import Buylist from "./Pages/Buylist";
 
 const Navigation = () => {
-    
+
     const [active, setActive] = React.useState("");
-    
+
+    const validURLs = ["spajz", "buylist"];
+
     useEffect(() => {
-        setActive(window.location.pathname.split("/")[1]);
+        var url: string = window.location.pathname.split("/")[1];
+        if (validURLs.includes(url)) {
+            setActive(url);
+        } else {
+            setActive("");
+            window.location.href = "/";
+        }
     }, [window.location.pathname]);
 
     return (
-        <body>
-        <Navbar className="navbar" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand onClick={() => setActive('spajz')} className={active === 'spajz' ? "navbar-link-active" : "navbar-link"} as={Link} to="/spajz">Špajzka</Navbar.Brand>
-                <Nav defaultActiveKey="/spajz" className="me-auto">
-                    <Nav.Link onClick={() => setActive('buylist')} className={active === 'buylist' ? "navbar-link-active" : "navbar-link"} as={Link} to="/buylist">Nákupní lístek</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar className="navbar" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand onClick={() => setActive('spajz')} className={active === 'spajz' ? "navbar-link-active" : "navbar-link"} as={Link} to="/spajz">Špajzka</Navbar.Brand>
+                    <Nav defaultActiveKey="/spajz" className="me-auto">
+                        <Nav.Link onClick={() => setActive('buylist')} className={active === 'buylist' ? "navbar-link-active" : "navbar-link"} as={Link} to="/buylist">Nákupní lístek</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
 
-        <Routes>
-            <Route path="/" element={<Navigate to="/spajz"/>}/>
-            <Route path="/spajz" element={<Spajz/>}/>
-            <Route path="/buylist" element={<Buylist/>}/>
-        </Routes>
-        </body>
+            <Routes>
+                <Route path="/" element={<Navigate to="/spajz"/>}/>
+                <Route path="/spajz" element={<Spajz/>}/>
+                <Route path="/buylist" element={<Buylist/>}/>
+            </Routes>
+        </>
     );
+
 }
 
 export default Navigation;
