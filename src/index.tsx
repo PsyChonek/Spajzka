1bf885928b83079cbc7d008cea89662aa7313598
Daniel <daniel.vazac@email.cz>
2022-11-08 16:07:05 +0100

    SkipWaiting

--- src/index.tsx
+++ src/index.tsx
@@ -26,17 +26,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-    onUpdate: (e) => {
-        const { waiting: { postMessage = null } = {} as any, update } = e || {};
-        if (postMessage) {
-            postMessage({ type: 'SKIP_WAITING' });
-        }
-        update().then(() => {
-            window.location.reload();
-        });
-    },
-});
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
