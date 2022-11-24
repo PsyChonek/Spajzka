61cf444852269bc0fd637f7f45fdaad9629e3959
Daniel Vazač <daniel.vazac@email.cz>
2022-11-24 14:05:34 +0100

    FiltersOptions.tsx

--- Spajzka.WEB/src/Components/Search/Buylist/ItemHead_Buylist.tsx
+++ Spajzka.WEB/src/Components/Search/Buylist/ItemHead_Buylist.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Container} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Button, ButtonGroup, Container, ToggleButton} from "react-bootstrap";
 
 function ItemHead_Buylist() {
     return (
         <Container className="searchContainer">
             <div className="gridContainer-head-buylist">
-                <div>Název</div>
-                <div>Je ve Špajzu?</div>
+                <ToggleButton id="toggle-check-name" type="checkbox" checked={filters.includes("name")} variant="primary" value="name" onChange={(e) => SelectFilterOption(e.currentTarget.value)}> Název </ToggleButton>
+                <ToggleButton id="toggle-check-inSpajz" type="checkbox" checked={filters.includes("inSpajz")} variant="primary" value="inSpajz" onChange={(e) => SelectFilterOption(e.currentTarget.value)}> Je ve Špajzu? </ToggleButton>
             </div>
         </Container>
     );
