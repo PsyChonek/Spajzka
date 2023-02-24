77914d18ec6bc956c7e5c2dec4cf57a698b47742
Daniel Vazač <daniel.vazac@email.cz>
2023-02-24 12:13:19 +0100

    Buttons

--- Spajzka.WEB/src/Components/Search/Buylist/ItemRow_Buylist.tsx
+++ Spajzka.WEB/src/Components/Search/Buylist/ItemRow_Buylist.tsx
@@ -9,7 +9,7 @@ const ItemRow_Buylist = (props: { item: Item, updateCallback: any }) => {
         SaveItem(props.item);
         props.updateCallback();
     }
-    
+
     const getRandomRotation = () => {
         return Math.floor(Math.random() * 360);
     }
@@ -18,7 +18,12 @@ const ItemRow_Buylist = (props: { item: Item, updateCallback: any }) => {
         <div className="gridContainer-item-buylist" onClick={() => UpdateItem(!props.item.isOnBuylist)}>
             {!props.item.isOnBuylist ? <div className="cross"></div> : <></>}
             <div className={props.item.isOnBuylist ? "" : "cross-text"}><b>{props.item.name.toLowerCase().charAt(0).toUpperCase() + props.item.name.slice(1).toLowerCase()}</b></div>
-            {   props.item.amount > 0 ? <>✅ ({props.item.amount})</> : <>❌</>}
+            {
+                props.item.amount > 0 ?
+                    <> <i className="bi bi-check-lg" style={{WebkitTextStrokeWidth: "0.8px", fontStyle: "normal"}}>
+                        ({props.item.amount})</i></> :
+                    <><i className="bi bi-x-lg" style={{WebkitTextStrokeWidth: "0.8px", fontStyle: "normal"}}></i></>
+            }
         </div>
     );
 }
