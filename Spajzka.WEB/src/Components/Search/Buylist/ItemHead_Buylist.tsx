52f5e7593766620f7a0062ba00b6e94cd2d99eab
Daniel Vazač <daniel.vazac@email.cz>
2023-03-06 14:47:18 +0100

    DropDown

--- Spajzka.WEB/src/Components/Search/Buylist/ItemHead_Buylist.tsx
+++ Spajzka.WEB/src/Components/Search/Buylist/ItemHead_Buylist.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Button, ButtonGroup, Container, ToggleButton} from "react-bootstrap";
+import {Button, ButtonGroup, Container, DropdownButton, ToggleButton} from "react-bootstrap";
 import {SortOptionsItem} from "../SortOptions";
 
 function ItemHead_Buylist(props: { sorts: SortOptionsItem[], updateSort: any }) {
@@ -7,27 +7,29 @@ function ItemHead_Buylist(props: { sorts: SortOptionsItem[], updateSort: any }) 
     return (
         <Container className="searchContainer">
             <div className="gridContainer-head-buylist">
-                <ToggleButton className="gridContainer-head-buylist-item" type="checkbox" value={"WHY"}
-                              checked={props.sorts?.find(x => x.value == "isOnBuylist")?.isActive}
-                              onClick={(e) => props.updateSort("isOnBuylist")}>
-                    V nákupním seznamu?
-                    {props.sorts?.find(x => x.value == "isOnBuylist")?.isDescending && <i className="bi-sort-down gridContainer-head-item-icon" ></i>
-                        || <i className="bi-sort-up gridContainer-head-item-icon"></i>}
-                </ToggleButton>
-                <ToggleButton className="gridContainer-head-buylist-item" type="checkbox" value={"WHY"}
-                              checked={props.sorts?.find(x => x.value == "name")?.isActive}
-                              onClick={(e) => props.updateSort("name")}>
-                    Název
-                    {props.sorts?.find(x => x.value == "name")?.isDescending && <i className="bi-sort-down gridContainer-head-item-icon"></i>
-                        || <i className="bi-sort-up gridContainer-head-item-icon"></i>}
-                </ToggleButton>
-                <ToggleButton className="gridContainer-head-buylist-item" type="checkbox" value={"WHY"}
-                              checked={props.sorts?.find(x => x.value == "inSpajz")?.isActive}
-                              onClick={(e) => props.updateSort("inSpajz")}>
-                    Je ve Špajzu?
-                    {props.sorts?.find(x => x.value == "inSpajz")?.isDescending && <i className="bi-sort-down gridContainer-head-item-icon"></i>
-                        || <i className="bi-sort-up gridContainer-head-item-icon"></i>}
-                </ToggleButton>
+                <DropdownButton title={"Seřadit"}>
+                    <ToggleButton className="gridContainer-head-buylist-item" type="checkbox" value={"WHY"}
+                                  checked={props.sorts?.find(x => x.value == "isOnBuylist")?.isActive}
+                                  onClick={(e) => props.updateSort("isOnBuylist")}>
+                        V nákupním seznamu?
+                        {props.sorts?.find(x => x.value == "isOnBuylist")?.isDescending && <i className="bi-sort-down gridContainer-head-item-icon" ></i>
+                            || <i className="bi-sort-up gridContainer-head-item-icon"></i>}
+                    </ToggleButton>
+                    <ToggleButton className="gridContainer-head-buylist-item" type="checkbox" value={"WHY"}
+                                  checked={props.sorts?.find(x => x.value == "name")?.isActive}
+                                  onClick={(e) => props.updateSort("name")}>
+                        Název
+                        {props.sorts?.find(x => x.value == "name")?.isDescending && <i className="bi-sort-down gridContainer-head-item-icon"></i>
+                            || <i className="bi-sort-up gridContainer-head-item-icon"></i>}
+                    </ToggleButton>
+                    <ToggleButton className="gridContainer-head-buylist-item" type="checkbox" value={"WHY"}
+                                  checked={props.sorts?.find(x => x.value == "inSpajz")?.isActive}
+                                  onClick={(e) => props.updateSort("inSpajz")}>
+                        Je ve Špajzu?
+                        {props.sorts?.find(x => x.value == "inSpajz")?.isDescending && <i className="bi-sort-down gridContainer-head-item-icon"></i>
+                            || <i className="bi-sort-up gridContainer-head-item-icon"></i>}
+                    </ToggleButton>
+                </DropdownButton>
             </div>
         </Container>
     );
