f615f1896e49754d80317c0fff611ed637f1e745
Daniel Vazač <daniel.vazac@email.cz>
2023-03-13 17:43:16 +0100

    swagger

--- Spajzka.API/index.js
+++ Spajzka.API/index.js
@@ -1,30 +1,65 @@
 ﻿const mongoose = require('mongoose');
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+// Load express and expose
 app.use(express.json());
-
 app.listen(3001, () => {
     console.log(`Server Started at ${3001}`)
 })
 
+
+const m2s = require('mongoose-to-swagger');
+const Item = require('./schema/item.ts')
+
+
+// Swagger setup
+swaggerOptions = {
+    swaggerDefinition: {
+        components: {
+            schemas: Item
+        },
+        info: {
+            title: 'Spajzka API',
+            description: 'Spajzka API Information',
+            contact: {
+                name: "Spajzka"
+            },
+
+            servers: ["http://localhost:3001"]
+        },
+        basePath: "/api"
+    },
+    apis: ["./routes/routes.ts"]
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Connect to DB
 const mongoConnectionsString = process.env.CONNECTION_STRING;
-
 mongoose.connect(mongoConnectionsString)
-
 const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-const Model = require('./models/model');
+// const Item = require('./schema/item.ts');
 //
-// const data = new Model({ 
-//     name: 'Test',
-//     age: 20
+// const data = new Item({
+//     name: "Okurka",
+//     price: 10,
+//     description: "Okurka",
+//     image: "Okurka",
+//     amount: 10,
+//     isOnBuylist: true
 // });
 //
-// const dataToSave = data.save();
+// data.save();
 
+// Load routes
+const routes = require('./routes/routes.ts');
+app.use('/api', routes);
