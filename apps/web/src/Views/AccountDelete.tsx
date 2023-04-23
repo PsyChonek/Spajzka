a042ca14cd1d3bcb65ae462a58c09eef981aea75
Daniel Vazač <daniel.vazac@email.cz>
2023-04-23 17:16:07 +0200

    Privacy, Account delete

---
+++ apps/web/src/Views/AccountDelete.tsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap"
+
+const AccountDelete = () => {
+    return (
+        <Container className="content">
+            <h1>Account Delete</h1>
+            <p> Guide users through the process of deleting their account. </p>
+            </Container>
+    )
+}
+
+export default AccountDelete
--- apps/web/src/Views/Navigation.tsx
+++ apps/web/src/Views/Navigation.tsx
@@ -4,12 +4,14 @@ import {Link, Navigate, Route, Routes} from "react-router-dom";
 import Spajz from "./Spajz";
 import Buylist from "./Buylist";
 import Apitest from './Apitest';
+import Privacy from './Privacy';
+import AccountDelete from './AccountDelete';
 
 const Navigation = () => {
 
     const [active, setActive] = React.useState("");
 
-    const validURLs = ["spajz", "buylist", "apitest", ""];
+    const validURLs = ["spajz", "buylist", "apitest", "", "privacy", "accountdelete"];
 
     useEffect(() => {
         var url: string = window.location.pathname.split("/")[1];
@@ -29,6 +31,8 @@ const Navigation = () => {
                     <Nav defaultActiveKey="/" className="me-auto">
                         <Nav.Link onClick={() => setActive('buylist')} className={active === 'buylist' ? "navbar-link-active" : "navbar-link"} as={Link} to="/buylist">Nákupní lístek</Nav.Link>
                         <Nav.Link onClick={() => setActive('apitest')} className={active === 'apitest' ? "navbar-link-active" : "navbar-link"} as={Link} to="/apitest">API</Nav.Link>
+                        <Nav.Link onClick={() => setActive('privacy')} className={active === 'privacy' ? "navbar-link-active" : "navbar-link"} as={Link} to="/privacy">Privacy</Nav.Link>
+                        <Nav.Link onClick={() => setActive('accountdelete')} className={active === 'accountdelete' ? "navbar-link-active" : "navbar-link"} as={Link} to="/accountdelete">Account Delete</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
@@ -38,6 +42,8 @@ const Navigation = () => {
                 <Route path="/spajz" element={<Spajz/>}/>
                 <Route path="/buylist" element={<Buylist/>}/>
                 <Route path="/apitest" element={<Apitest/>}/>
+                <Route path="/privacy" element={<Privacy/>}/>
+                <Route path="/accountdelete" element={<AccountDelete/>}/>
             </Routes>
         </>
     );
